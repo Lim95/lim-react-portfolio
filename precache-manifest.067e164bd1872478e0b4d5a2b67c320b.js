58a654c115b6d27e0bbac7f7638389cb26c25395
Lim95 <sungjoon.lim@gmail.com>
2021-04-24 00:56:35 -0700

    Updates

---
+++ precache-manifest.067e164bd1872478e0b4d5a2b67c320b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7c5b839f6f43332d1fae412a6391bd",
+    "url": "/lim-react-portfolio/index.html"
+  },
+  {
+    "revision": "35b20351f253e791dc2f",
+    "url": "/lim-react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ad8fbf899a8139543bee",
+    "url": "/lim-react-portfolio/static/css/main.6e4b4069.chunk.css"
+  },
+  {
+    "revision": "35b20351f253e791dc2f",
+    "url": "/lim-react-portfolio/static/js/2.03af814c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lim-react-portfolio/static/js/2.03af814c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8fbf899a8139543bee",
+    "url": "/lim-react-portfolio/static/js/main.aa52d3d6.chunk.js"
+  },
+  {
+    "revision": "77284656adc1ae703047",
+    "url": "/lim-react-portfolio/static/js/runtime-main.f9508336.js"
+  }
+]);
